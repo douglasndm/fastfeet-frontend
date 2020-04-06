fca56c4a13aebc4e7ff771116173b7f934a6c38f
Douglas Nunes de Mattos <DouglasNunesdeMattos@outlook.com>
2020-04-06 01:20:27 -0300

    Utilizando o Unform

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,22 +1,27 @@
 import React from 'react';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '../../assets/fastfeet-logo.svg';
 import { Container, LoginForm } from './styles';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+
   return (
     <Container>
       <LoginForm>
         <img src={logo} alt="Fast Feet" />
 
-        <form>
+        <Form onSubmit={handleSubmit}>
           <strong>SEU E-MAIL</strong>
-          <input type="email" placeholder="exemplo@email.com" />
+          <Input name="email" type="email" placeholder="exemplo@email.com" />
           <strong>SUA SENHA</strong>
-          <input type="password" placeholder="***********" />
+          <Input name="password" type="password" placeholder="***********" />
 
           <button type="submit">Entrar no sistema</button>
-        </form>
+        </Form>
       </LoginForm>
     </Container>
   );
